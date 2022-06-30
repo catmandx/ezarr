90f785817e962d9622a1474e01abc52cf66ee1c5
Hoang Nguyen <50922013+catmandx@users.noreply.github.com>
2022-06-30 14:26:44 +0700

    Update setup.sh
    Removed lidarr
    Refactored some commands for reliability and replicability on multiple systems.

--- setup.sh
+++ setup.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 
 # Make users and group
-sudo useradd sonarr
-sudo useradd radarr
-sudo useradd lidarr
-sudo useradd prowlarr
-sudo useradd qbittorrent
-sudo groupadd mediacenter
+sudo useradd sonarr -u 13001
+sudo useradd radarr -u 13002
+sudo useradd prowlarr -u 13003
+sudo useradd qbittorrent -u 13004
+sudo groupadd mediacenter -g 13000
 sudo usermod -a -G mediacenter sonarr
 sudo usermod -a -G mediacenter radarr
-sudo usermod -a -G mediacenter lidarr
 sudo usermod -a -G mediacenter prowlarr
 sudo usermod -a -G mediacenter qbittorrent
 
 # Make directories
-sudo mkdir docker data
-sudo mkdir docker/sonarr-config docker/radarr-config docker/lidarr-config docker/prowlarr-config docker/qbittorrent-config
-sudo mkdir data/torrents data/media
-sudo mkdir data/torrents/tv data/torrents/movies data/torrents/music data/media/tv data/media/movies data/media/music
+sudo mkdir -pv docker/{sonarr,radarr,prowlarr,qbittorrent}-config
+sudo mkdir -pv data/{torrents,data}/{tv,movies}
 
 # Set permissions
 sudo chmod -R 775 data/
-sudo chown -R 1000:mediacenter data/
+sudo chown -R $(id -u):mediacenter data/
 sudo chown -R sonarr:mediacenter docker/sonarr-config
 sudo chown -R radarr:mediacenter docker/radarr-config
-sudo chown -R lidarr:mediacenter docker/lidarr-config
 sudo chown -R prowlarr:mediacenter docker/prowlarr-config
 sudo chown -R qbittorrent:mediacenter docker/qbittorrent-config
 
-sonarr_id=$(sudo cat /etc/passwd | grep sonarr | cut -c 10-13)
-radarr_id=$(sudo cat /etc/passwd | grep radarr | cut -c 10-13)
-lidarr_id=$(sudo cat /etc/passwd | grep lidarr | cut -c 10-13)
-prowlarr_id=$(sudo cat /etc/passwd | grep prowlarr | cut -c 12-15)
-qbit_id=$(sudo cat /etc/passwd | grep qbittorrent | cut -c 15-18)
-group_id=$(sudo cat /etc/group | grep mediacenter | cut -c 15-18)
+sonarr_id=$(id sonarr -u)
+radarr_id=$(id radarr -u)
+prowlarr_id=$(id prowlarr -u)
+qbit_id=$(id qbittorrent -u)
+group_id=$(getent group adm | cut -d: -f3)
 
 echo "SONARR_UID=${sonarr_id}" >> .env
 echo "RADARR_UID=${radarr_id}" >> .env
